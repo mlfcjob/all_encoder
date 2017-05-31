7c73dc8991a5e6c6a6010b8abdd663b60282620e
zettakit <zettakit@host-192-168-103-139.openstacklocal>
2017-05-31 18:55:00 +0800

    add cross compile file

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd ./build
+
+
+if [ $1 == "win32" ]
+then
+echo "Target windows 32 lib."
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolChain_win32.cmake ..
+fi
+
+if [ $1 == "win64" ]
+then
+echo "Target windows 64 lib"
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolChain_win64.cmake ..
+fi
+
+if [ $1 == "linux" ]
+then
+echo "Target linux lib."
+cmake ..
+fi
+
+make -j8
+make install
+
